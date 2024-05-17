d53075cb30b05f0a62c74105c216c921c0e8d90f
snjnaa <Sanjanaab.ag@gmail.com>
2024-05-17 15:33:28 +0530

    Create Back.jsx

---
+++ src/component/common/back/Back.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min'
+
+const Back = ({title}) => {
+    const location = useLocation()
+    return (
+        <div>
+            <section className="back">
+                <h2>Home / {location.pathname.split("/")[1]} </h2>
+                <h1>{title}</h1>
+            </section>
+            <div className="margin">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Back
